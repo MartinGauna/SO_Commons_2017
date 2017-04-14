fd2ee68c5f60f2139af184d47afd47c9a81aaf2f
joseVirgili <virgili.josemaria@gmail.com>
2017-04-14 20:57:53 -0300

    handShake funcion Cliente y Servidor, implementacion en fileSystem,Memoria con Kernel

--- Consola/src/sockets.h
+++ Consola/src/sockets.h
@@ -6,5 +6,6 @@ void create_server(int* listenningSocket, char* port);
 int accept_connection(int listenningSocket, int* socketCliente);
 
 void create_client(int* serverSocket, char* ip, char* port);
-
+void handShakeServidor(int serverSocket,const char * servidor,const char * cliente);
+void handShakeCliente(int clienteSocket,const char * servidor,const char * cliente);
 #endif /* SOCKETS_H_ */
